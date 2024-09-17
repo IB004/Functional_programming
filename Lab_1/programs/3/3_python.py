ce578695317d7bea78ce449e9034a7d15341b626
IB004 <butov.main@gmail.com>
2024-09-17 15:18:19 +0300

    3 python solve

---
+++ Lab_1/programs/3/3_python.py
@@ -0,0 +1,29 @@
+def largest_prime_factor(x):
+    if is_prime(x):
+        return x
+    divisor = 2
+    while divisor < x // 2 + 1:
+        if x == divisor:
+            return divisor
+        if x % divisor == 0:
+            return largest_prime_factor(x // divisor)
+        divisor = get_next_prime(divisor)
+
+def get_next_prime(x):
+    x += 1
+    while not is_prime(x):
+        x += 1
+    return x
+
+def is_prime(x):
+    if x < 2:
+        return False
+    divisor = 2
+    while divisor < x // 2 + 1:
+        if x % divisor == 0:
+            return False
+        divisor += 1
+    return True
+
+x = 600851475143
+print(largest_prime_factor(x))
