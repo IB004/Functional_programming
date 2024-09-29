81271575d13fb72b8892e817d3b9572a79b38cff
IB004 <butov.main@gmail.com>
2024-09-29 18:42:11 +0300

    28 format

--- Lab_1/programs/28.hs
+++ Lab_1/programs/28.hs
@@ -1,48 +1,55 @@
-diagSumRecursion len = _diagSumRecursion 1 0 (len^2) where
-    _diagSumRecursion x delta maxX | x > maxX   = 0
-                                   | isCorner x = x + _diagSumRecursion (x + delta + 2) (delta + 2) maxX
-                                   | otherwise  = x + _diagSumRecursion (x + delta) delta maxX
+diagSumRecursion len = _diagSumRecursion 1 0 (len ^ 2)
+  where
+    _diagSumRecursion x delta maxX
+      | x > maxX = 0
+      | isCorner x = x + _diagSumRecursion (x + delta + 2) (delta + 2) maxX
+      | otherwise = x + _diagSumRecursion (x + delta) delta maxX
 
 
-diagSumTailRecursion len = _diagSumTailRecursion 1 0 0 (len^2) where
-    _diagSumTailRecursion x delta acc maxX | x > maxX   = acc
-                                           | isCorner x = _diagSumTailRecursion (x + delta + 2) (delta + 2) (acc + x) maxX
-                                           | otherwise  = _diagSumTailRecursion (x + delta) delta (acc + x) maxX
+diagSumTailRecursion len = _diagSumTailRecursion 1 0 0 (len ^ 2)
+  where
+    _diagSumTailRecursion x delta acc maxX
+      | x > maxX = acc
+      | isCorner x = _diagSumTailRecursion (x + delta + 2) (delta + 2) (acc + x) maxX
+      | otherwise = _diagSumTailRecursion (x + delta) delta (acc + x) maxX
 
 
-diagSumWithSequence len = foldr1 (+) $ filter isOnDiag [1..(len^2)]
+diagSumWithSequence len = foldr1 (+) $ filter isOnDiag [1 .. (len ^ 2)]
 
 
-diagSumMap len = sum $ map (\x -> if isOnDiag x then x else 0) [1..(len^2)]
+diagSumMap len = sum $ map (\x -> if isOnDiag x then x else 0) [1 .. (len ^ 2)]
 
 
-diagSumInfiniteList len = sum $ map fst $ take ((len `div` 2) * 4 + 1) $ iterate getNextDiag (1, 0) 
+diagSumInfiniteList len = sum $ map fst $ take ((len `div` 2) * 4 + 1) $ iterate getNextDiag (1, 0)
 
 
-getNextDiag (x, delta) | isCorner x = (x + delta + 2, delta + 2)
-                       | otherwise  = (x + delta, delta)
+getNextDiag (x, delta)
+  | isCorner x = (x + delta + 2, delta + 2)
+  | otherwise = (x + delta, delta)
 
 
 isOnDiag :: Integer -> Bool
-isOnDiag x | isCorner x = True
-           | x == corner - 1 * delta = True
-           | x == corner - 2 * delta = True
-           | x == corner - 3 * delta = True
-           | otherwise = False
-    where
+isOnDiag x
+  | isCorner x = True
+  | x == corner - 1 * delta = True
+  | x == corner - 2 * delta = True
+  | x == corner - 3 * delta = True
+  | otherwise = False
+  where
     corner = closesetCorner x
     delta = floorIntSqrt corner - 1
-           
 
 
 closesetCorner :: Integer -> Integer
-closesetCorner x | isCorner x = x
-                 | otherwise  = (xCut + 1 + xCut `mod` 2)^2 where 
-                    xCut = floorIntSqrt x
+closesetCorner x
+  | isCorner x = x
+  | otherwise = (xCut + 1 + xCut `mod` 2) ^ 2
+  where
+    xCut = floorIntSqrt x
 
 
 isCorner :: Integer -> Bool
-isCorner x = x `mod` 2 == 1 && x == (floorIntSqrt x)^2
+isCorner x = x `mod` 2 == 1 && x == (floorIntSqrt x) ^ 2
 
 
 floorIntSqrt :: Integer -> Integer
