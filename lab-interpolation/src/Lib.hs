27d3f4c5a771edbbc9465c7a5dbcf152ef01a38b
IB004 <butov.main@gmail.com>
2025-01-17 15:39:16 +0300

    .basic read cycle

--- lab-interpolation/src/Lib.hs
+++ lab-interpolation/src/Lib.hs
@@ -1,8 +1,31 @@
 module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Text.Read(readMaybe)
+import Control.Monad (unless)
 
+data Window a = Window [a] deriving (Show, Eq)
+
+emptyWindow = Window []
+
+addLeft x (Window win)  = Window $ x : win
+
+readToWindow wind = do 
+    putStrLn $ show wind
+    input <- readDot
+    unless (input == "exit") $ do
+        let xy = extractNumbers input
+        readToWindow $ addLeft xy wind
+
+someFunc :: IO ()
+someFunc = putStrLn "Hello!"
+
+readNumbers :: IO [Float]
+readNumbers = do
+      n <- getLine
+      case (n, readMaybe n :: Maybe Float) of
+        ("end", _) -> return []
+        (_, Just n) -> (n:) <$> readNumbers
+        (_, Nothing) -> readNumbers
 
 readDot = do
     putStr "Enter x y: "
@@ -16,7 +39,7 @@ extractNumbers xy = (read x, read y)
     x = head lst
     y = head $ tail lst
 
-
+hw :: IO ()
 hw = do
     putStrLn "What is your name?"
     name <- getLine
@@ -28,4 +51,3 @@ hw = do
 putStr' :: String -> IO ()
 putStr' "" = return ()
 putStr' (c:cs) = putChar c >> putStr' cs
-
